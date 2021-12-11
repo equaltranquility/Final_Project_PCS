edca303ecba14099128b27e007c68c5a1096b755
Alexey Kozlov <aleksey31099@gmail.com>
2021-12-11 11:35:05 +0700

    Layout edits

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -4,56 +4,52 @@ const Contact = () => {
   return (
     <div className="content">
       <div className="container">
-        <div>
-          <div className="content__title">
-            <h1>Вопросы, отзывы и предложения</h1>
-            <hr />
-          </div>
-        </div>
+        <h1 className="content__title">Вопросы, отзывы и предложения</h1>
 
-          <div>
-            <form>
-              <div>
-                <label for="NameForm" className="form-label">
-                  ФИО
-                </label>
-                <input
-                  type="text"
-                  class="form-control"
-                  id="NameForm"
-                  placeholder="Иван Иванов"
-                  required
-                />
-              </div>
-              <div>
-                <label for="FormControlInput1" className="form-label">
-                  Email
-                </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="FormControlInput1"
-                  placeholder="name@example.com"
-                  required
-                />
-              </div>
-              <div>
-                <label for="FormControlTextarea" className="form-label">
-                  Напишите ваше мнение
-                </label><br/>
-                <textarea
-                  class="form-control"
-                  id="FormControlTextarea"
-                  rows="5"
-                  required
-                ></textarea>
-              </div>
-              <br/>
-              <button type="submit" className="button-submit">
-                Отправить сообщение
-              </button>
-            </form>
-          </div>
+        <div>
+          <form>
+            <div>
+              <label for="NameForm" className="form-label">
+                ФИО
+              </label>
+              <input
+                type="text"
+                class="form-control"
+                id="NameForm"
+                placeholder="Иван Иванов"
+                required
+              />
+            </div>
+            <div>
+              <label for="FormControlInput1" className="form-label">
+                Email
+              </label>
+              <input
+                type="email"
+                className="form-control"
+                id="FormControlInput1"
+                placeholder="name@example.com"
+                required
+              />
+            </div>
+            <div>
+              <label for="FormControlTextarea" className="form-label">
+                Напишите ваше мнение
+              </label>
+              <br />
+              <textarea
+                class="form-control"
+                id="FormControlTextarea"
+                rows="5"
+                required
+              ></textarea>
+            </div>
+            <br />
+            <button type="submit" className="button-submit">
+              Отправить сообщение
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
